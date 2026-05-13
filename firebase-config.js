6bdfa5e2833ca0abbb52371a22522c4b22df03a4
aguiar <aguiar.br2006@gmail.com>
2026-05-13 15:26:12 -0300

    zerado para rayssa

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,17 @@
-﻿// Firebase configuration - Using compat SDK (loaded via script tags in index.html)
+// Firebase configuration - Using compat SDK (loaded via script tags in index.html)
 // No need for imports - firebase global object is available from script tags
 
 // Your web app's Firebase configuration
-window.BARBARABEAUTY_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBfwCRrvq-WPlcTkwNxrnMLI8ThQOGxW9E",
-  authDomain: "barbarabeauty-5d75a.firebaseapp.com",
-  projectId: "barbarabeauty-5d75a",
-  storageBucket: "barbarabeauty-5d75a.firebasestorage.app",
-  messagingSenderId: "207260630604",
-  appId: "1:207260630604:web:eef19636976d1bd4e37ee9",
-  measurementId: "G-M4DKXD38NW"
+window.RAYSSA_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDwH3VhiWpzYRas-QXzVxNuA0-_wW7g5sE",
+  authDomain: "rayssaoliveira-b9c86.firebaseapp.com",
+  projectId: "rayssaoliveira-b9c86",
+  storageBucket: "rayssaoliveira-b9c86.firebasestorage.app",
+  messagingSenderId: "983397980222",
+  appId: "1:983397980222:web:75c6765109f537c0d7196f",
+  measurementId: "G-XBX5ZNPTFZ"
 };
 
 // Document path for syncing across devices
-window.BARBARABEAUTY_FIREBASE_DOC_PATH = "sistemas/BARBARABEAUTY";
+window.RAYSSA_FIREBASE_DOC_PATH = "sistemas/rayssa-oliveira";
 
