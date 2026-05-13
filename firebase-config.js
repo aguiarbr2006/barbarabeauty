2688d948390efe7a7ce78966b9900d8800a41295
aguiar <aguiar.br2006@gmail.com>
2026-05-13 08:36:19 -0300

    iden

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,17 @@
 ﻿// Firebase configuration - Using compat SDK (loaded via script tags in index.html)
 // No need for imports - firebase global object is available from script tags
 
-window.BARBARA_FIREBASE_CONFIG = {
-  apiKey: "SUA_API_KEY_AQUI",
-  authDomain: "barbarabeauty.firebaseapp.com",
-  projectId: "barbarabeauty",
-  storageBucket: "barbarabeauty.appspot.com",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-  appId: "SEU_APP_ID",
-  measurementId: "SEU_MEASUREMENT_ID",
+// Your web app's Firebase configuration
+window.RAYSSA_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBfwCRrvq-WPlcTkwNxrnMLI8ThQOGxW9E",
+  authDomain: "barbarabeauty-5d75a.firebaseapp.com",
+  projectId: "barbarabeauty-5d75a",
+  storageBucket: "barbarabeauty-5d75a.firebasestorage.app",
+  messagingSenderId: "207260630604",
+  appId: "1:207260630604:web:eef19636976d1bd4e37ee9",
+  measurementId: "G-M4DKXD38NW"
 };
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(window.BARBARA_FIREBASE_CONFIG);
-}
+// Document path for syncing across devices
+window.RAYSSA_FIREBASE_DOC_PATH = "sistemas/rayssa-oliveira";
 
-window.BARBARA_FIREBASE_DOC_PATH = "sistemas/barbarabeauty";
