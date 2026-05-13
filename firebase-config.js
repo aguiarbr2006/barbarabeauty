547318052f280b63b84c17fa57e67ac35cc6c92f
aguiar <aguiar.br2006@gmail.com>
2026-05-13 08:29:01 -0300

    iden

--- firebase-config.js
+++ firebase-config.js
@@ -2,13 +2,13 @@
 // No need for imports - firebase global object is available from script tags
 
 window.BARBARA_FIREBASE_CONFIG = {
-  apiKey: "SUA_API_KEY_AQUI",
-  authDomain: "barbarabeauty.firebaseapp.com",
-  projectId: "barbarabeauty",
-  storageBucket: "barbarabeauty.appspot.com",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-  appId: "SEU_APP_ID",
-  measurementId: "SEU_MEASUREMENT_ID",
+ apiKey: "AIzaSyBfwCRrvq-WPlcTkwNxrnMLI8ThQOGxW9E",
+  authDomain: "barbarabeauty-5d75a.firebaseapp.com",
+  projectId: "barbarabeauty-5d75a",
+  storageBucket: "barbarabeauty-5d75a.firebasestorage.app",
+  messagingSenderId: "207260630604",
+  appId: "1:207260630604:web:eef19636976d1bd4e37ee9",
+  measurementId: "G-M4DKXD38NW",
 };
 
 if (!firebase.apps.length) {
