f3e80c8b35797e14c761e7f27c61c931ac8c4b79
aguiar <aguiar.br2006@gmail.com>
2026-05-13 14:20:01 -0300

    fire

--- firebase-config.js
+++ firebase-config.js
@@ -2,28 +2,7 @@
 // No need for imports - firebase global object is available from script tags
 
 // Your web app's Firebase configuration
-// window.RAYSSA_FIREBASE_CONFIG = {
-//   apiKey: "AIzaSyBfwCRrvq-WPlcTkwNxrnMLI8ThQOGxW9E",
-//   authDomain: "barbarabeauty-5d75a.firebaseapp.com",
-//   projectId: "barbarabeauty-5d75a",
-//   storageBucket: "barbarabeauty-5d75a.firebasestorage.app",
-//   messagingSenderId: "207260630604",
-//   appId: "1:207260630604:web:eef19636976d1bd4e37ee9",
-//   measurementId: "G-M4DKXD38NW"
-};
-
-// Document path for syncing across devices
-// window.RAYSSA_FIREBASE_DOC_PATH = "sistemas/rayssa-oliveira";
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+window.BARBARABEAUTY_FIREBASE_CONFIG = {
   apiKey: "AIzaSyBfwCRrvq-WPlcTkwNxrnMLI8ThQOGxW9E",
   authDomain: "barbarabeauty-5d75a.firebaseapp.com",
   projectId: "barbarabeauty-5d75a",
@@ -33,6 +12,6 @@ const firebaseConfig = {
   measurementId: "G-M4DKXD38NW"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Document path for syncing across devices
+window.BARBARABEAUTY_FIREBASE_DOC_PATH = "sistemas/barbarabeauty";
+
